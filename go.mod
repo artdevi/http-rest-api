1f63f0faa9365263bcde91f3adc3c757cfd62142
artdevi <tar.19@uni-dubna.ru>
2022-01-21 12:03:20 +0300

    added store

--- go.mod
+++ go.mod
@@ -9,11 +9,16 @@ require (
 	github.com/stretchr/testify v1.7.0
 )
 
-require github.com/lib/pq v1.10.4
+require (
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/lib/pq v1.10.4
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+)
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -12,9 +12,7 @@ func TestUserRepository_Create(t *testing.T) {
 	s, teardown := store.TestStore(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@email.com",
-	})
+	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
@@ -25,10 +23,12 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 
 	email := "user@example.org"
 	_, err := s.User().FindByEmail(email)
-
 	assert.Error(t, err)
 
-	u, err := s.User().FindByEmail(email)
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
